cec9434efd11ac15860303c1ee40d34d13db6c5f
Jake Soenneker <jake@soenneker.com>
2025-08-15 10:21:43 -0500

    Initial commit

---
+++ src/RangePointsPairOverride.cs
@@ -0,0 +1,17 @@
+﻿using Soenneker.Dtos.RangePointsPairs;
+using Soenneker.Utils.AutoBogus.Context;
+using Soenneker.Utils.AutoBogus.Override;
+
+namespace Soenneker.AutoFaker.Overrides.RangePointsPairs;
+
+/// <summary>
+/// An AutoFaker (AutoBogus) override for the object RangePointsPair
+/// </summary>
+public sealed class RangePointsPairOverride : AutoFakerOverride<RangePointsPairOverride>
+{
+    public override void Generate(AutoFakerOverrideContext context)
+    {
+        var target = (context.Instance as RangePointsPair)!;
+        target.Points = context.Faker.Random.Int(0, 20);
+    }
+}
---
+++ test/Soenneker.AutoFaker.Overrides.RangePointsPairs.Tests/RangePointsPairOverrideTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.AutoFaker.Overrides.RangePointsPairs.Tests;
+
+public sealed class RangePointsPairOverrideTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
